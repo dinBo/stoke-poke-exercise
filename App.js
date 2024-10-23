5f414a3e85d5e93eac8ba252c982bec07df4aa3b
Bodin Todorovic <bodin93@gmail.com>
2024-10-23 19:49:32 +0200

    chore: adding localisation

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
+import { getLocales } from 'expo-localization';
+import { I18n } from 'i18n-js';
+
+const translations = {
+  en: { welcome: 'Hello', name: 'Bodin' },
+  es: { welcome: 'Bienvenido', name: 'Bodin' },
+};
+const i18n = new I18n(translations);
+
+i18n.locale = getLocales()[0].languageCode ?? 'en';
+
+i18n.enableFallback = true;
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>
+        {i18n.t('welcome')} {i18n.t('name')}
+      </Text>
+      <Text>Current locale: {i18n.locale}</Text>
+      <Text>Device locale: {getLocales()[0].languageCode}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    flex: 1,
+  },
+  text: {
+    fontSize: 20,
+    marginBottom: 16,
   },
 });
